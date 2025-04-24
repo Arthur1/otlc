38167c6fe2e0f0bcef20cc48bb3e9b928613d285
ASAKURA Kazuki <32762324+Arthur1@users.noreply.github.com>
2025-04-25 00:44:13 +0900

    chore: check error on fmt.Fprintf call

--- internal/cli/versioncmd.go
+++ internal/cli/versioncmd.go
@@ -12,15 +12,23 @@ import (
 type VersionCmd struct{}
 
 func (c *VersionCmd) Run(globals *Globals) error {
-	printVersion()
-	return nil
+	return printVersion()
 }
 
-func printVersion() {
+func printVersion() error {
 	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
-	fmt.Fprintf(writer, "otlc is a Command-line Tool to Export Telemetry by OTLP (OpenTelemetry Protocol).\n")
-	fmt.Fprintf(writer, "Version:\t%s\n", otlc.Version)
-	fmt.Fprintf(writer, "Go version:\t%s\n", runtime.Version())
-	fmt.Fprintf(writer, "Arch:\t%s\n", runtime.GOARCH)
+	if _, err := fmt.Fprintf(writer, "otlc is a Command-line Tool to Export Telemetry by OTLP (OpenTelemetry Protocol).\n"); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(writer, "Version:\t%s\n", otlc.Version); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(writer, "Go version:\t%s\n", runtime.Version()); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(writer, "Arch:\t%s\n", runtime.GOARCH); err != nil {
+		return err
+	}
 	writer.Flush()
+	return nil
 }
